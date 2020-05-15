3b78a074f3022c141d4b5264968d5f6a7df39479
Randi Adiel Gianufian <randiadiel30@gmail.com>
2020-05-16 01:04:28 +0700

    Add bullet paralax animation

---
+++ dist/js/index.js
@@ -0,0 +1,21 @@
+import "../js/jquery/jquery.min.js";
+
+$(document).ready(function ($) {
+  $("#mousearea").mousemove(function (e) {
+    let height = e.pageY;
+    const width = e.pageX;
+
+    let top = $(window).scrollTop();
+    height -= top;
+    // console.log(`${height - top} ${width}`);
+    const moveY = height / 15;
+    const moveX = width / 15;
+    $(".bullet-one").css({ transform: `translate(${moveX}px, ${moveY}px)` });
+    $(".bullet-two").css({
+      transform: `translate(${moveX / 2}px, ${moveY / 2}px)`,
+    });
+    $(".bullet-three").css({
+      transform: `translate(${moveX / 3}px, ${moveY / 3}px)`,
+    });
+  });
+});
