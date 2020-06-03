69fb19591b54552d5e409a0fce9c8bdd4712113d
Jeremy Yonathan Chrisyanto <Jychrisyanto5@gmail.com>
2020-06-03 19:10:15 +0700

    create operator page, 2 error in console

--- dist/js/index.js
+++ dist/js/index.js
@@ -2,18 +2,18 @@ import "../js/jquery/jquery.min.js";
 
 const eventContent = [
   {
-  title : "event-1",
-  content : `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`
+    title: "event-1",
+    content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`,
   },
   {
-    title : "event-2",
-    content : `Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.`
+    title: "event-2",
+    content: `Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.`,
   },
   {
-    title : "event-3",
-    content : `Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?`
-  }
-]
+    title: "event-3",
+    content: `Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?`,
+  },
+];
 
 $(document).ready(function ($) {
   let scrollPos = window.pageYOffset;
@@ -44,90 +44,180 @@ $(document).ready(function ($) {
     $(".bullet-three").css({
       transform: `translate(${moveX / 3}px, ${moveY / 3}px)`,
     });
-
   });
 
-    //event-carousel
-  
+  //event-carousel
+
   const imageCount = 3;
   const initDots = (imageCount) => {
-    for(let i = 1 ; i <= imageCount ; i++){
-      document.getElementById('carousel-dot-list').innerHTML += `
+    for (let i = 1; i <= imageCount; i++) {
+      document.getElementById("carousel-dot-list").innerHTML += `
       <div class="carousel-dot" id="dot-${i}"></div>
-      `
+      `;
     }
-  }
+  };
 
   const initImage = (imageCount) => {
-    for(let i = 1 ; i <= imageCount ; i++){
-      document.getElementById('carousel-image-list').innerHTML += `
+    for (let i = 1; i <= imageCount; i++) {
+      document.getElementById("carousel-image-list").innerHTML += `
       <img src="../dist/assets/carousel-${i}.png" class="carousel-image" id="carousel-${i}" alt="carousel-${i}">
       `;
     }
-  }
+  };
 
   const activateCarouselItem = (index) => {
-    document.getElementById(`dot-${index}`).classList.add('carousel-dot-active')
-    document.getElementById(`carousel-${index}`).classList.add('carousel-image-active')
-  } 
+    document
+      .getElementById(`dot-${index}`)
+      .classList.add("carousel-dot-active");
+    document
+      .getElementById(`carousel-${index}`)
+      .classList.add("carousel-image-active");
+  };
 
   const deactivateCarouselItem = (index) => {
-    document.getElementById(`dot-${index}`).classList.remove('carousel-dot-active')
-    document.getElementById(`carousel-${index}`).classList.remove('carousel-image-active')
-  }
+    document
+      .getElementById(`dot-${index}`)
+      .classList.remove("carousel-dot-active");
+    document
+      .getElementById(`carousel-${index}`)
+      .classList.remove("carousel-image-active");
+  };
 
-
-  const initCarousel = (imageCount) =>{
+  const initCarousel = (imageCount) => {
     initDots(imageCount);
     initImage(imageCount);
     activateCarouselItem(1);
     changeEventContent(0);
-  }
+  };
 
   const changeEventContent = (index) => {
     $(".carousel-heading").html(eventContent[index].title);
     $(".carousel-paragraph").html(eventContent[index].content);
-  }
+  };
 
   const getActiveItemIndex = () => {
-    return $("#carousel-image-list").children(".carousel-image-active").index()+1;
-  }
+    return (
+      $("#carousel-image-list").children(".carousel-image-active").index() + 1
+    );
+  };
 
   $("#arrow-next").click((e) => {
     const index = getActiveItemIndex();
-    if(index == imageCount){
-      return
-    }else{
-      activateCarouselItem(index+1)
-      deactivateCarouselItem(index)
-      changeEventContent(index)
+    if (index == imageCount) {
+      return;
+    } else {
+      activateCarouselItem(index + 1);
+      deactivateCarouselItem(index);
+      changeEventContent(index);
     }
   });
 
   $("#arrow-prev").click((e) => {
     const index = getActiveItemIndex();
-    if(index == 0){
-      return
-    }else{
-      activateCarouselItem(index-1)
-      deactivateCarouselItem(index)
-      changeEventContent(index-2)
+    if (index == 0) {
+      return;
+    } else {
+      activateCarouselItem(index - 1);
+      deactivateCarouselItem(index);
+      changeEventContent(index - 2);
     }
   });
 
   const changeActive = (newActive, oldActive) => {
     activateCarouselItem(newActive);
     deactivateCarouselItem(oldActive);
-    changeEventContent(newActive-1);
-  }
-  
-  initCarousel(imageCount)
- 
+    changeEventContent(newActive - 1);
+  };
+
+  initCarousel(imageCount);
+
   $(".carousel-dot").click((e) => {
-    const newActivateIndex = $(".carousel-dots").children(`#${e.target.id}`).index() + 1;
-    const oldActivateIndex = $(".carousel-dots").children(`.carousel-dot-active`).index() + 1
-    changeActive(newActivateIndex,oldActivateIndex)
-  })
+    const newActivateIndex =
+      $(".carousel-dots").children(`#${e.target.id}`).index() + 1;
+    const oldActivateIndex =
+      $(".carousel-dots").children(`.carousel-dot-active`).index() + 1;
+    changeActive(newActivateIndex, oldActivateIndex);
+  });
 
+  //operator
+  const track = document.querySelector(".operator__carousel__track");
+  const slides = Array.from(track.children);
+  const nextButton = document.querySelector(
+    ".operator__carousel__button--right"
+  );
+  const prevButton = document.querySelector(
+    ".operator__carousel__button--left"
+  );
+  const dotsNav = document.querySelector(".operator__carousel__nav");
+  const dots = Array.from(dotsNav.children);
+
+  const slideWidth = slides[0].getBoundingClientRect().width;
+
+  slides.forEach(setSlidePosition);
+
+  const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + "px";
+  };
+
+  const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = "translateX(-" + targetSlide.style.left + ")";
+    currentSlide.classList.remove("operator__current-slide");
+    targetSlide.classList.add("operator__current-slide");
+  };
+
+  const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove("operator__current-slide");
+    targetDot.classList.add("operator__current-slide");
+  };
+
+  const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+    if (targetIndex === 0) {
+      prevButton.classList.add("operator__is-hidden");
+      nextButton.classList.remove("operator__is-hidden");
+    } else if (targetIndex === slides.length - 1) {
+      prevButton.classList.remove("operator__is-hidden");
+      nextButton.classList.add("operator__is-hidden");
+    } else {
+      prevButton.classList.remove("operator__is-hidden");
+      nextButton.classList.remove("operator__is-hidden");
+    }
+  };
+
+  //when click left
+  prevButton.addEventListener("click", (e) => {
+    const currentSlide = track.querySelector(".operator__current-slide");
+    const prevSlide = currentSlide.previousElementSibling;
+    const currentDot = dotsNav.querySelector(".operator__current-slide");
+    const prevDot = currentDot.previousElementSibling;
+    const prevIndex = slides.findIndex((slide) => slide === prevSlide);
+    moveToSlide(track, currentSlide, prevSlide);
+    updateDots(currentDot, prevDot);
+    hideShowArrows(slides, prevButton, nextButton, prevIndex);
+  });
+  //when click right
+  nextButton.addEventListener("click", (e) => {
+    const currentSlide = track.querySelector(".operator__current-slide");
+    const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotsNav.querySelector(".operator__current-slide");
+    const nextDot = currentDot.nextElementSibling;
+    const nextIndex = slides.findIndex((slide) => slide === nextSlide);
+    moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
+    hideShowArrows(slides, prevButton, nextButton, nextIndex);
+  });
+
+  //when click nav
+  dotsNav.addEventListener("click", (e) => {
+    const targetDot = e.target.closest("button");
+    if (!targetDot) return;
+
+    const currentSlide = track.querySelector(".operator__current-slide");
+    const currentDot = dotsNav.querySelector(".operator__current-slide");
+    const targetIndex = dots.findIndex((dot) => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+    hideShowArrows(slides, prevButton, nextButton, targetIndex);
+  });
 });
-
